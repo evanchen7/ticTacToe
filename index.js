7958732dcea3bf8aad0ed560d7e495eb56ab647b
Evan Chen <evanchen7@yahoo.com>
2017-11-13 14:49:18 -0800

    'created tictactoe function names'

---
+++ index.js
@@ -0,0 +1,36 @@
+// * Minimal UI that redraws the board and makes clear whose turn it is, each turn.
+// * Players can submit moves (assume, admittedly unrealistically, that both players are sitting at the same keyboard).
+// * Win detection - detect and display who won
+
+// Start a game
+var TicTacToe = function () {
+
+};
+
+// Create a board
+
+var generateNewBoard = function () {
+  this.board = [['*', '*', '*'],
+                ['*', '*', '*'],
+                ['*', '*', '*']];
+};
+
+// Toggle pieces with X
+var toggleX = function (x, y) {
+
+};
+
+// Toggle pieces with O
+var toggleO = function (x, y) {
+
+};
+
+// Collision detection
+var checkCollision = function () {
+
+};
+
+// Win detection
+var winDetection = function () {
+
+};
