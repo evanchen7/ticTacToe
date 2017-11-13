30d35fd6e998f18166fa5f2e94e4628cd82db4b9
Evan Chen <evanchen7@yahoo.com>
2017-11-13 15:30:15 -0800

    'only row condition win'

--- index.js
+++ index.js
@@ -18,32 +18,52 @@ var generateNewBoard = function () {
 // Toggle board
 TicTacToe.prototype = {
     toggleX: function (x, y) {
-      if (x <= 2 && y <= 2) {
+      if (x <= 2 && y <= 2 && this.checkCollision(x, y)) {
         this.board[x][y] = 'X';
+        this.winDetection();
         this.showBoard();
       } else {
-        throw 'Input valid ranges for X and Y';
+        throw 'Input valid ranges or piece collision' && this.showBoard();
       }
     },
-    toggleY: function (x, y) {
-      if (x <= 2 && y <= 2) {
-        this.board[x][y] = 'Y';
+    toggleO: function (x, y) {
+      if (x <= 2 && y <= 2 && this.checkCollision(x, y)) {
+        this.board[x][y] = 'O';
+        this.winDetection();
         this.showBoard();
       } else {
-        throw 'Input valid ranges for X and Y';
+        throw 'Input valid ranges or piece collision' && this.showBoard();
       }
     },
     showBoard: function() {
        return this.board;
+    },
+    checkCollision: function (x, y) {
+       return this.board[x][y] === '*';
+    },
+    winDetection: function () {
+      var xResults = [];
+      var oResults = [];
+      var rowX = 0;
+      var rowO = 0;
+
+      // All three cells in any row are the same
+      for (var i = 0; i < this.board.length; i++) {
+          if (this.board[0][i] === 'O') {
+              rowO++;
+          }
+          if (this.board[0][j] === 'X') {
+              rowX++;
+          }
+      }
+      if (rowX === 3) {
+          return 'X is the winner';
+      }
+      if (rowO === 3) {
+          return 'O is the winner';
+      }
+
+      // All three cells in any column are the same
+      // All three cells traversing the board diagonally are the same.
     }
-};
-
-// Collision detection
-var checkCollision = function () {
-
-};
-
-// Win detection
-var winDetection = function () {
-
 };
